751735ab9e0f2752e90937fef55b43b7ab14efda
vishvassolanki <vishvassolanki08@gmail.com>
2025-06-01 10:20:32 +0530

    Variables and Datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; // Treat all JS code as newer JS version
+
+let username = "Vishvas";
+let age = 19;
+let city;
+let isLoggedIn = true;
+let num = BigInt(123);
+let language = null;
+// console.log(num);
+
+console.log(typeof city);
+console.log(typeof language);
---
+++ Basics/variable.js
@@ -0,0 +1,22 @@
+var name = "Vishvas";
+let email = "solanki@gmail.com";
+const city = "Sutrapada";
+id = 123;
+
+// console.log(city);
+// city = "Kodinar";
+// console.log(city);
+
+// console.log(email);
+// email = "vishvas@google.com";
+// console.log(email);
+
+// console.log(name);
+// name = "Mayur";
+// console.log(name);
+
+// console.log(id);
+// id = 112233;
+// console.log(id);
+
+console.table([id, name, email, city]);
