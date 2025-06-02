9268a67b83d9f39aff3f17a2707de2806462520b
vishvassolanki <vishvassolanki08@gmail.com>
2025-06-02 12:52:36 +0530

    Learn about type conversion

---
+++ Basics/typeConversion.js
@@ -0,0 +1,55 @@
+let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// True => 1
+// False => 0
+// Null => 0
+// "Vishvas" => NaN
+// undefined => NaN
+
+let isLoggedIn = null;
+
+let valueInBoolean = Boolean(isLoggedIn);
+
+// console.log(typeof valueInBoolean);
+// console.log(valueInBoolean);
+
+let num = "Vishvas";
+
+let valueInString = String(num);
+
+// console.log(valueInString);
+// console.log(typeof valueInString);
+
+let value = 3;
+
+let negativeValue = -value;
+
+// console.log(value);
+// console.log(negativeValue);
+
+// let str1 = "Hello";
+// let str2 = " World";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log(1 + 2);
+
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+// console.log(1 + "2" + "3");
+// console.log("1" + "2" + 3);
+
+// console.log(false + true + 2);
