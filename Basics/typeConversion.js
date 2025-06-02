156da6c2378d4658929e40f5528c8cd58952d8b6
vishvassolanki <vishvassolanki08@gmail.com>
2025-06-02 19:50:48 +0530

    Learn about comparisons

--- Basics/typeConversion.js
+++ Basics/typeConversion.js
@@ -53,3 +53,15 @@ let negativeValue = -value;
 // console.log("1" + "2" + 3);
 
 // console.log(false + true + 2);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
