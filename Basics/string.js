0d95f531e0149a8221a29cb01ec4b56ec260ef3c
vishvassolanki <vishvassolanki08@gmail.com>
2025-06-03 11:26:16 +0530

    Strings and Methods

---
+++ Basics/string.js
@@ -0,0 +1,58 @@
+// console.log("My name is" + " Vishvas Solanki");
+
+// let username = "Vishvas";
+// let city = "Gir Somnath";
+
+// console.log(username + " is from " + city);
+
+// console.log(
+//     `Hello my name is ${username.toUpperCase()} and I am from ${city.toUpperCase()}.`
+// );
+
+// let carName = new String("Fortuner");
+
+// console.log(typeof username);
+// console.log(typeof carName);
+
+// console.log(carName.toUpperCase());
+// console.log(carName);
+
+// let newCar = carName;
+// console.log(newCar.slice(3));
+// console.log(carName);
+
+// console.log(username.charAt(2));
+// console.log(username.indexOf("s"));
+
+// const newName = username;
+
+// console.log(newName.length);
+// console.log(newName.substring(0, 6));
+// console.log(newName.slice(-5));
+
+// const newStr = "      Solanki      ";
+
+// console.log(newStr.length);
+// console.log(newStr.trim());
+
+// const newName2 = "Mayur";
+
+// console.log(newName2.replace("Ma", "Ke"));
+// console.log(newName2.replaceAll("Ke"));
+
+// const url = "https://www.vishvas%20solanki.com";
+
+// console.log(url.replace("%20", "-"));
+// console.log(url.includes("com"));
+
+// let newString = "Vishvas-Solanki-Sara-Sutrapada";
+
+// console.log(newString.split("-"));
+
+let str = "Solanki";
+// let a = "a";
+// let b = "b";
+
+// console.log(a < b);
+// console.log(str.charAt(0));
+// console.log(str.at(0, 2));
