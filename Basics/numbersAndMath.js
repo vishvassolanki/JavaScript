2b6f9848f49f3ad60d6811f71956a0af0016b2ba
vishvassolanki <vishvassolanki08@gmail.com>
2025-06-03 12:19:42 +0530

    Numbers and Maths

---
+++ Basics/numbersAndMath.js
@@ -0,0 +1,46 @@
+// const num1 = 10;
+
+// const num2 = new Number(200);
+
+// console.log(num1);
+// console.log(num2);
+
+// console.log(num1.toString());
+// console.log(num1.toFixed(2));
+// console.log(num1.toExponential(2));
+// console.log(typeof num1.toLocaleString());
+// console.log(num1.toPrecision(4));
+// console.log(num1.valueOf());
+
+// const newNum = 145.896;
+
+// console.log(newNum.toPrecision(3));
+
+// const bigNum = 1500000;
+
+// console.log(bigNum.toLocaleString());
+
+// console.log(Math);
+// console.log(Math.abs(-8));
+// console.log(Math.round(8.12));
+// console.log(Math.ceil(8.12));
+// console.log(Math.floor(8.92));
+
+// console.log(Math.min(1, 2, 3, 4, 5));
+// console.log(Math.max(1, 2, 3, 4, 5));
+
+// const randomNumber = (Math.random() * 10) + 1;
+
+// console.log(randomNumber);
+// console.log(randomNumber);
+
+
+
+// Random Number Formula
+
+const min = 10;
+const max = 20;
+
+const random = Math.floor(Math.random() * (max - min + 1) + min);
+
+console.log(random);
